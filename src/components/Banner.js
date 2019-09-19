e981f4994b8f489ec876e3fb532acb98ad1d97da
Ral Mayer <llev4n@gmail.com>
2019-09-19 15:08:16 +0300

    more cleanup

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Banner = () => 
+    <section id="banner">
+        <div className="inner">
+            <h2>Spectral</h2>
+            <p>Another fine responsive<br />
+                site template freebie<br />
+                crafted by <a href="http://html5up.net">HTML5 UP</a>.</p>
+            <ul className="actions special">
+                <li><a href="#" className="button primary">Activate</a></li>
+            </ul>
+        </div>
+        <a href="#one" className="more scrolly">Learn More</a>
+    </section>
+
+
+export default Banner

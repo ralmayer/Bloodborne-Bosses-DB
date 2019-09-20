da0d0b17123b66d327dfe082708a2ebe19bfbd7d
Ral Mayer <llev4n@gmail.com>
2019-09-20 12:15:30 +0300

    first steps in creating app skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.scss';
-import Nav from './components/Nav';
-import Banner from './components/Banner';
+import Nav from './components/Nav'
+import Banner from './components/Banner'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
 
 function App() {
     return (
         <div>
             <Nav />
             <Banner />
+            <CTA />
+            <Footer />
         </div>
     )
     }

ea00acbfd35975bb8557e8fc943e28719f86095c
Ral Mayer <llev4n@gmail.com>
2019-09-20 17:28:50 +0300

    added burger button without functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 const Nav = () => 
 <div id='nav'>
-    <ul>
-        <li>Home</li>
-        <li>Bosses</li>
-        <li>About</li>
+    <div id='logo'>
+        Logo
+    </div>
+    <ul id='nav-links'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Bosses</a></li>
+        <li><a href='#'>About</a></li>
     </ul>
+    <div id='burger'>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 </div>
 
 export default Nav

feeaef91174448a44f85860251d0d5e695b235ed
Ral Mayer <llev4n@gmail.com>
2019-09-22 16:01:21 +0300

    rewrote nav scssto resolve intruction interference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import Nav from './components/Nav'
 import Banner from './components/Banner'
 import CTA from './components/CTA'
 import Footer from './components/Footer'

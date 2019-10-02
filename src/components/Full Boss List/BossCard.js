72ee0ab8f3bba4fe2853560dd0eccb2c87de0728
Ral Mayer <llev4n@gmail.com>
2019-10-02 14:09:53 +0300

    working on dynamic routing

--- src/components/Full Boss List/BossCard.js
+++ src/components/Full Boss List/BossCard.js
@@ -1,31 +1,32 @@
 import React, { useContext, Fragment } from 'react'
 import { BossesContext } from '../contexts/BossesContext'
+import { Link } from 'react-router-dom'
 
-const BossCard = () => {
+const BossCard = (props) => {
 
     const { bossesList } = useContext(BossesContext)
 
     return (
         <Fragment>
-            { bossesList ? bossesList.bosses.map((boss, index) => 
-            <div className='boss-card' key={boss.id}>
-                <div className='boss-card-avatar' id={boss.id}>
-                    <img src={boss.avatar} alt={boss.id}/>
-                    <div className='avatar-menu'>
-                       <div id='avatarMenuTitle'>
-                       <p>{boss.name}</p>
-                       </div>
-                       <div className='break'></div>
-                        <a href='#'>
-                            <div>
-                                learn more
+            {bossesList ? bossesList.bosses.map((boss, index) =>
+                <div className='boss-card' key={boss.id}>
+                    <div className='boss-card-avatar' id={boss.id}>
+                        <img src={boss.avatar} alt={boss.id} />
+                        <div className='avatar-menu'>
+                            <div id='avatarMenuTitle'>
+                                <p>{boss.name}</p>
                             </div>
-                        </a>
+                            <div className='break'></div>
+                            <Link to={`/bosses/${boss.id}`}>
+                                <div>
+                                    learn more
+                                </div>
+                            </Link>
+                        </div>
                     </div>
+                    <div className='boss-card-description-box'></div>
                 </div>
-                <div className='boss-card-description-box'></div>
-            </div>
-        ) : null}
+            ) : null}
         </Fragment>
     )
 }

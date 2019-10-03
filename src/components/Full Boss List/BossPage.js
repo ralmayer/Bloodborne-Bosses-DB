57d8124a9fc60af5448e44099d882b056514a568
Ral Mayer <llev4n@gmail.com>
2019-10-03 15:19:52 +0300

    changed repo tag

--- src/components/Full Boss List/BossPage.js
+++ src/components/Full Boss List/BossPage.js
@@ -1,11 +1,9 @@
-import React, { Fragment, useState, useEffect, useContext } from 'react'
+import React, { Fragment, useContext } from 'react'
 import { BossesContext } from '../contexts/BossesContext'
 
 const BossPage = ({match: {params: {id}}}) => {
     
     const { bossesList } = useContext(BossesContext)
-
-    console.log(bossesList ? bossesList.bosses.filter(obj => obj.id === id)[0].name : null)
 
     return (
         

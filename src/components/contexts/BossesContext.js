3d79c08414c52ae3d3ca41ecc110cd41ca4d4e55
Ral Mayer <llev4n@gmail.com>
2019-10-06 16:32:02 +0300

    trying to publish to github pages

--- src/components/contexts/BossesContext.js
+++ src/components/contexts/BossesContext.js
@@ -6,7 +6,7 @@ const BossesContextProvider = (props) => {
     const [bossesList, setBossesList] = useState(null)
 
     const fetchData = async () => {
-        const res = await fetch('https://api.myjson.com/bins/evpjj')
+        const res = await fetch('https://api.myjson.com/bins/1fuemv')
         const data = await res.json()
         setBossesList(data)
     }

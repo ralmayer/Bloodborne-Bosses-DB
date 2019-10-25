2ef5524a791ae69f04ceeedeb6142999bd93db0e
Ral Mayer <llev4n@gmail.com>
2019-10-25 15:42:36 +0400

    integrated auth with general routing, nav element reacts to changes in auth state

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react'
+import React, { useContext, Fragment } from 'react'
 import firebase from '../firebase'
 import { AuthContext } from '../contexts/AuthContext'
+import Nav from '../nav/Nav'
 
 const Login = () => {
 
@@ -10,7 +11,7 @@ const Login = () => {
         try {
             await firebase.login(email, password).then(res => setUser(res.user))
 
-         } catch (error) { alert(error.message) }
+        } catch (error) { alert(error.message) }
     }
 
     async function logout() {
@@ -18,41 +19,42 @@ const Login = () => {
     }
 
     return (
-        <div>
-        <form>
-            <input
-                type="email"
-                value={email}
-                name="email"
-                placeholder="email"
-                onChange={e => { setEmail(e.target.value) }}
-                required
-            />
+        <Fragment>
+            <Nav />
+            <form>
+                <input
+                    type="email"
+                    value={email}
+                    name="email"
+                    placeholder="email"
+                    onChange={e => { setEmail(e.target.value) }}
+                    required
+                />
+                <br />
+                <input
+                    type="password"
+                    value={password}
+                    name="password"
+                    placeholder="password"
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+                <br />
+                <button onClick={e => {
+                    e.preventDefault()
+                    login()
+                    setEmail('')
+                    setPassword('')
+                }}>Submit</button>
+                <br />
+                <button onClick={e => {
+                    e.preventDefault()
+                    logout().then(setUser(''))
+                }}>Sign Out</button>
+            </form>
             <br />
-            <input
-                type="password"
-                value={password}
-                name="password"
-                placeholder="password"
-                onChange={e => setPassword(e.target.value)}
-                required
-            />
-            <br />
-            <button onClick={e => {
-                e.preventDefault()
-                login()
-                setEmail('')
-                setPassword('')
-            }}>Submit</button>
-            <br />
-            <button onClick={e => {
-                e.preventDefault()
-                logout().then(setUser(''))
-            }}>Sign Out</button>
-        </form>
-        <br />
-        <div>{user ? `Welcome, ${user.displayName}` : 'not logged in'}</div>
-        </div>
+            <div>{user ? `Welcome, ${user.displayName}` : 'not logged in'}</div>
+        </Fragment>
     )
 }
 

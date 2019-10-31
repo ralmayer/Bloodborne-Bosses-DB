0964d86e051bccffd72bc0c18712e68da37fa874
Ral Mayer <llev4n@gmail.com>
2019-10-31 17:19:59 +0400

    removed auth context

--- src/components/Shouts.js
+++ src/components/Shouts.js
@@ -2,10 +2,12 @@ import React, { useContext, Fragment } from 'react'
 import { Comments } from './Comments'
 import Nav from './nav/Nav'
 
-export const Shouts = () => {
+const Shouts = () => {
     return <Fragment>
         <Nav />
         <h1>Shouts Components</h1>
         <Comments pageName='shouts'/>
     </Fragment>
 }
+
+export default Shouts
--- src/components/contexts/AuthContext.js
+++ src/components/contexts/AuthContext.js
@@ -1,26 +1,26 @@
-import React, { createContext, useState, useEffect } from 'react'
-import firebase from '../firebase'
-import App from '../../App'
+// import React, { createContext, useState, useEffect } from 'react'
+// import firebase from '../firebase'
+// import App from '../../App'
 
-export const AuthContext = createContext()
+// export const AuthContext = createContext()
 
-const AuthContextProvider = (props) => {
+// const AuthContextProvider = (props) => {
 
-    const [cred, setCred] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+//     const [cred, setCred] = useState('')
+//     const [email, setEmail] = useState('')
+//     const [password, setPassword] = useState('')
 
-    useEffect(() => {
-        firebase.isInitialized().then(val => setCred(val))
-    })
+//     useEffect(() => {
+//         firebase.isInitialized().then(val => setCred(val))
+//     })
  
 
-    return (
-        <AuthContext.Provider value={{ cred, setCred, email, setEmail, password, setPassword}}>
-            {/* <App /> */}
-            {props.children}
-        </AuthContext.Provider>
-    )
-}
+//     return (
+//         <AuthContext.Provider value={{ cred, setCred, email, setEmail, password, setPassword}}>
+//             {/* <App /> */}
+//             {props.children}
+//         </AuthContext.Provider>
+//     )
+// }
 
-export default AuthContextProvider;
+// export default AuthContextProvider;

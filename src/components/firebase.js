6af3866e442f6f1b5ce05c4b9a66a2c9024d173f
Leon (Ral) Mayer <48362631+ralmayer@users.noreply.github.com>
2019-12-03 15:08:33 +0400

    removed api keys

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth'
 import 'firebase/firebase-firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBKsW2MNHtfmjU-Gk_euDPQJIBEaI-yqV8",
-    authDomain: "bloodbornedb.firebaseapp.com",
-    databaseURL: "https://bloodbornedb.firebaseio.com",
-    projectId: "bloodbornedb",
-    storageBucket: "bloodbornedb.appspot.com",
-    messagingSenderId: "1060896385450",
-    appId: "1:1060896385450:web:e96c9491c9a3ca196ba3d3",
-    measurementId: "G-NYR6LS4V95"
+    apiKey: 
+    authDomain: 
+    databaseURL: 
+    projectId:
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
+    measurementId: 
 };
 
 class Firebase {
